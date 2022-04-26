ea9b91ab00e0bb9d1fc815b5d7665d0cb7b6bcd7
Alan Ibarra <alan.ibarra209@gmail.com>
2022-04-26 16:20:03 -0700

    Added types for navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,8 +1,25 @@
 import Image from "next/image";
-interface Props {
-  screen: "mobile" | "tablet";
+type Screen = "mobile" | "tablet" | "desktop";
+
+interface LogoType {
+  type: "logo";
+  size: Exclude<Screen, "desktop">;
 }
 
+interface NavType {
+  type: "nav";
+  size: Screen;
+}
+
+interface AvatarType {
+  type: "avatar";
+  size: Screen;
+}
+
+type NavBarElements = LogoType | AvatarType | NavType;
+
+// Added redundant nav property just so I can apply a larger gap
+// at desktop size.
 const screens = {
   mobile: {
     logo: { width: 25, height: 20 },
@@ -12,110 +29,112 @@ const screens = {
   tablet: {
     logo: { width: 32, height: 25 },
     nav: { width: 20, height: 20 },
-    avatar: { width: 32, height: 32 },
+    avatar: { width: 40, height: 40 },
+  },
+  desktop: {
+    avatar: { width: 40, height: 40 },
+    nav: { width: 20, height: 20 },
   },
 };
+
 const Nav = () => {
   return (
-    <div className="bg-semiDarkBlue flex items-center h-14 px-4 border-2 border-red  md:h-16  md:px-6 md:rounded-lg xl:flex-col  xl:w-24 xl:h-full xl:p-0 xl:py-8 ">
-      <div>
-        {/* Mobile Logo */}
-        <div className="md:hidden flex items-center">
-          <Image
-            src="/icons/logo.svg"
-            width={screens.mobile.logo.width}
-            height={screens.mobile.logo.width}
-            alt="logo"
-          />
+    <div className="md:px-6  xl:p-0 xl:py-8 xl:pl-8 ">
+      <div className="bg-semiDarkBlue flex items-center h-14  px-4 md:h-16 md:rounded-lg xl:flex-col xl:w-24 xl:h-full xl:px-0 xl:py-8 xl:rounded-2xl">
+        {/* Logo */}
+        {/* Flex to get rid of the extra space for the image on the bottom */}
+        <div className=" flex md:hidden items-center ">
+          {NavLayout({ type: "logo", size: "mobile" })}
         </div>
-        {/* Tablet Logo */}
         <div className="hidden md:flex items-center">
-          <Image
-            src="/icons/logo.svg"
-            width={screens.tablet.logo.width}
-            height={screens.tablet.logo.width}
-            alt="logo"
-          />
+          {NavLayout({ type: "logo", size: "tablet" })}
         </div>
-      </div>
-      <div className="flex-1">
-        {/* Mobile Nav */}
-        <div className="flex justify-center gap-6  md:hidden">
-          <Image
-            src="/icons/icon-nav-home.svg"
-            width={screens.mobile.nav.width}
-            height={screens.mobile.nav.height}
-            alt="Home"
-          />
-          <Image
-            src="/icons/icon-nav-movies.svg"
-            width={screens.mobile.nav.width}
-            height={screens.mobile.nav.height}
-            alt="Movies"
-          />
-          <Image
-            src="/icons/icon-nav-tv-series.svg"
-            width={screens.mobile.nav.width}
-            height={screens.mobile.nav.height}
-            alt="Tv series"
-          />
-          <Image
-            src="/icons/icon-nav-bookmark.svg"
-            width={screens.mobile.nav.width}
-            height={screens.mobile.nav.height}
-            alt="Bookmarked movies"
-          />
+
+        {/* Nav  */}
+        <div className="flex-1  border-2 border-white">
+          <div className="md:hidden">
+            {NavLayout({ type: "nav", size: "mobile" })}
+          </div>
+          <div className="hidden md:block xl:hidden">
+            {NavLayout({ type: "nav", size: "tablet" })}
+          </div>
+          <div className="hidden xl:block xl:h-full">
+            {NavLayout({ type: "nav", size: "desktop" })}
+          </div>
         </div>
-        {/* Tablet Nav */}
-        <div className="hidden md:flex justify-center gap-8 xl:flex-col xl:h-full">
-          <Image
-            src="/icons/icon-nav-home.svg"
-            width={screens.tablet.nav.width}
-            height={screens.tablet.nav.height}
-            alt="Home"
-          />
-          <Image
-            src="/icons/icon-nav-movies.svg"
-            width={screens.tablet.nav.width}
-            height={screens.tablet.nav.height}
-            alt="Movies"
-          />
-          <Image
-            src="/icons/icon-nav-tv-series.svg"
-            width={screens.tablet.nav.width}
-            height={screens.tablet.nav.height}
-            alt="Tv series"
-          />
-          <Image
-            src="/icons/icon-nav-bookmark.svg"
-            width={screens.tablet.nav.width}
-            height={screens.tablet.nav.height}
-            alt="Bookmarked movies"
-          />
+        {/* Avatar */}
+        <div className="md:hidden">
+          {NavLayout({ type: "avatar", size: "mobile" })}
         </div>
-      </div>
-      <div>
-        {/* Mobile Avatar */}
-        <div className="flex items-center md:hidden ">
-          <Image
-            src="/icons/image-avatar.png"
-            width={screens.mobile.avatar.width}
-            height={screens.mobile.avatar.height}
-            alt="image avatar"
-          />
+        <div className="hidden md:block xl:hidden">
+          {NavLayout({ type: "avatar", size: "tablet" })}
         </div>
-        {/* Tablet Avatar */}
-        <div className="hidden md:flex items-center">
-          <Image
-            src="/icons/image-avatar.png"
-            width={screens.tablet.avatar.width}
-            height={screens.tablet.avatar.height}
-            alt="image avatar"
-          />
+        <div className="hidden xl:block">
+          {NavLayout({ type: "avatar", size: "desktop" })}
         </div>
       </div>
     </div>
   );
 };
+
+function NavLayout(navBarElement: NavBarElements): JSX.Element {
+  const { type, size } = navBarElement;
+  switch (type) {
+    case "logo": {
+      return (
+        <Image
+          src="/icons/logo.svg"
+          width={screens[size][type].width}
+          height={screens[size][type].height}
+          alt="logo"
+        />
+      );
+    }
+    case "nav": {
+      return (
+        <div
+          className={`flex justify-center gap-6 md:gap-8  xl:h-full xl:gap-10 xl:flex-col`}
+        >
+          <Image
+            src="/icons/icon-nav-home.svg"
+            width={screens[size][type].width}
+            height={screens[size][type].height}
+            alt="Home"
+          />
+          <Image
+            src="/icons/icon-nav-movies.svg"
+            width={screens[size][type].width}
+            height={screens[size][type].height}
+            alt="Movies"
+          />
+          <Image
+            src="/icons/icon-nav-tv-series.svg"
+            width={screens[size][type].width}
+            height={screens[size][type].height}
+            alt="Tv series"
+          />
+          <Image
+            src="/icons/icon-nav-bookmark.svg"
+            width={screens[size][type].width}
+            height={screens[size][type].height}
+            alt="Bookmarked movies"
+          />
+        </div>
+      );
+    }
+    case "avatar": {
+      return (
+        <div className="flex items-center">
+          <Image
+            src="/icons/image-avatar.png"
+            width={screens[size][type].width}
+            height={screens[size][type].height}
+            alt="image avatar"
+          />
+        </div>
+      );
+    }
+  }
+}
 
 export default Nav;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,8 @@ import Nav from "../components/Nav";
 const Home: NextPage = () => {
   return (
     <div className="h-full flex flex-col gap-6 xl:flex-row xl:gap-9 ">
-      <div className="md:p-6 md:pb-0 xl:pr-0 xl:pb-6  border-white border-2">
-        <Nav />
-      </div>
+      <Nav />
+
       <div className="border-4 border-red flex-1"></div>
     </div>
   );
