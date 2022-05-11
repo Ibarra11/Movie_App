99983c5112df86bc2e90cf1f67c0ee2af6131bbc
Alan Ibarra <alan.ibarra209@gmail.com>
2022-05-11 14:19:02 -0700

    grid layout for desktop

--- components/MovieGrid.tsx
+++ components/MovieGrid.tsx
@@ -6,7 +6,7 @@ const MovieGrid = () => {
   return (
     <div className="border-2 border-white">
       <h4 className="text-white mb-4">Recommended for you</h4>
-      <div className="grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-x-7 md:gap-y-6 ">
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-x-7 md:gap-y-6 lg:grid-cols-4 lg: gap-x-10 gap-y-8   ">
         {RegularThumbnails.map((movie) => {
           return <Thumbnail key={movie.title} {...movie} />;
         })}
