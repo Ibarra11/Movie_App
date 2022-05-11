62b0e778906675c82afe40940154186cf4328c77
Alan Ibarra <alan.ibarra209@gmail.com>
2022-05-11 14:15:45 -0700

    Change grid layout for tablet
    - 3 columns for tablet
    - readjust the aspect ratio
    - change the column and row gap for tablet

--- components/MovieGrid.tsx
+++ components/MovieGrid.tsx
@@ -1,13 +1,15 @@
 import Thumbnail from "./Thumbnail";
+import data from "../data.json";
+import type { Movie } from "./TrendingRow";
+const RegularThumbnails = data.filter((movie) => !movie.isTrending) as Movie[];
 const MovieGrid = () => {
   return (
     <div className="border-2 border-white">
       <h4 className="text-white mb-4">Recommended for you</h4>
-      <div className="grid grid-cols-2  gap-4">
-        <Thumbnail />
-        <Thumbnail />
-        <Thumbnail />
-        <Thumbnail />
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-x-7 md:gap-y-6 ">
+        {RegularThumbnails.map((movie) => {
+          return <Thumbnail key={movie.title} {...movie} />;
+        })}
       </div>
     </div>
   );
