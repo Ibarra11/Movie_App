89f6ce931a22d2d066022165a13e0df24f5a9c2f
Alan Ibarra <alan.ibarra209@gmail.com>
2022-05-10 21:08:20 -0700

    Add trending row and the recommended section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from "next";
 import Nav from "../components/Nav";
 import Input from "../components/Input";
+import TrendingRow from "../components/TrendingRow";
+import MovieGrid from "../components/MovieGrid";
 const Home: NextPage = () => {
   return (
-    <div className="h-full flex flex-col gap-6 xl:flex-row xl:gap-9 ">
+    <div className="h-full flex flex-col gap-6 md:gap-9 xl:flex-row xl:gap-9 ">
       <Nav />
-      <div className="border-4 px-4 border-red flex-1">
+      <div className="border-4 px-4 border-red flex-1 md:px-6">
         <Input />
+        <TrendingRow />
+        <MovieGrid />
       </div>
     </div>
   );
