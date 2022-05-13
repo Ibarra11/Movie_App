f7fbf875693114accb9911d07e2581dd875786bb
Alan Ibarra <alan.ibarra209@gmail.com>
2022-05-12 17:29:04 -0700

    change from height screen to min-height screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="bg-darkBlue h-full pb-14">
+    <div className="bg-darkBlue min-h-screen pb-14">
       <Component {...pageProps} />{" "}
     </div>
   );
