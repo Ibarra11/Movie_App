f5b121b5dffc3f715f0e524421666800e5af0e64
Alan Ibarra <alan.ibarra209@gmail.com>
2022-05-27 18:05:06 -0700

    changed the logic when a page is protected

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,30 +6,24 @@ import apolloClient from "../lib/apollo";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
-
-  if (pageProps.protected) {
-    fetch("/api/auth/user")
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.isLoggedIn === false) {
-          router.push("/account/login");
-        } else {
-          <div className="bg-darkBlue min-h-screen pb-14">
-            <ApolloProvider client={apolloClient}>
-              <Component {...pageProps} />
-            </ApolloProvider>
-          </div>;
-        }
-      });
-  } else {
-    return (
-      <div className="bg-darkBlue min-h-screen pb-14">
-        <ApolloProvider client={apolloClient}>
-          <Component {...pageProps} />
-        </ApolloProvider>
-      </div>
-    );
+  async function checkLoginStatus() {
+    const res = await fetch("/api/auth/user");
+    const data = await res.json();
+    if (data.isLoggedIn === false) {
+      router.push("/account/login");
+    }
   }
+  if (pageProps.protected) {
+    checkLoginStatus();
+  }
+
+  return (
+    <div className="bg-darkBlue min-h-screen pb-14">
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </div>
+  );
 }
 
 export default MyApp;
