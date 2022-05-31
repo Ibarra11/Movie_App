47dbce1069ad32ffbd039c80d3eb63616639c183
Alan Ibarra <alan.ibarra209@gmail.com>
2022-05-30 17:10:43 -0700

    moved the input down on desktop

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 const Input = () => {
   return (
-    <form className="relative flex items-center w-full cursor-pointer isolate overflow-hidden">
+    <div className="relative mt-16 -translate-y-1/3  flex items-center w-full cursor-pointer isolate overflow-hidden">
       <div className="flex absolute">
         <Image
           src="/icons/icon-search.svg"
@@ -11,7 +11,7 @@ const Input = () => {
         />
       </div>
 
-      <div className="relative w-full z-10 ">
+      <div className="relative w-full z-10">
         <input
           className="peer appearance-none cursor-pointer bg-transparent  w-full pl-10 py-1 pr-1 border-none text-base text-white caret-red font-light focus:outline-none"
           placeholder="Search for movies or Tv series"
@@ -19,7 +19,7 @@ const Input = () => {
         />
         <div className="absolute invisible peer-focus:visible bottom-0 h-px w-[calc(100%_-_theme(spacing.10))] bg-greyishBlue translate-x-10"></div>
       </div>
-    </form>
+    </div>
   );
 };
 
