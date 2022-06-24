92c7ec3499e44c64e7c05476e8f8a4d9d0af56a7
Alan Ibarra <alan.ibarra209@gmail.com>
2022-06-23 19:22:43 -0700

    change placeholder value based on route

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import React, { Dispatch, SetStateAction } from "react";
+const routes: any = {
+  "/": "movies or Tv Series",
+  "/movies": "movies",
+  "/tv_series": "TV series",
+  "/bookmarked": "bookmarked shows",
+};
 const Input: (props: {
   setSearchValue: Dispatch<SetStateAction<string>>;
   searchValue: string;
 }) => React.ReactElement = ({ setSearchValue, searchValue }) => {
+  const { pathname } = useRouter();
   return (
     <div className="relative mt-16 -translate-y-1/3  flex items-center w-full cursor-pointer isolate overflow-hidden">
       <div className="flex absolute">
@@ -18,7 +26,7 @@ const Input: (props: {
       <div className="relative w-full z-10">
         <input
           className="peer appearance-none cursor-pointer bg-transparent  w-full pl-10 py-1 pr-1 border-none text-base text-white caret-red font-light focus:outline-none"
-          placeholder="Search for movies or Tv series"
+          placeholder={`Search for ${routes[pathname]}`}
           type="text"
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
