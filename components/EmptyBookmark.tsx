c970306829d88932346104f80f81c5193c0e937c
Alan Ibarra <alan.ibarra209@gmail.com>
2022-06-29 19:45:38 -0700

    add a text prop to empty bookmark to display different messages

--- components/EmptyBookmark.tsx
+++ components/EmptyBookmark.tsx
@@ -1,9 +1,11 @@
 import { BiMovie } from "react-icons/bi";
-const EmptyBookmark = () => {
+const EmptyBookmark: (props: {
+  text: "TV Series" | "Movie";
+}) => React.ReactElement = ({ text }) => {
   return (
     <div className="absolute  flex flex-col gap-6 w-full h-full justify-center items-center text-white">
       <BiMovie color="white" size={72} />
-      <p className=" text-lg">Bookmark a movie to view it here</p>
+      <p className=" text-lg">Bookmark a {text} to view it here</p>
     </div>
   );
 };
